31d33d05a301d53c91e2a57025fffc124c9cf15b
Lee <leewhiz119@gmail.com>
2022-10-24 16:01:54 -0400

    hooked up redux and added test component

---
+++ src/redux/rootReducer.js
@@ -0,0 +1,13 @@
+const initState = {
+    posts: [
+        {id: '1', title: 'nveonvoevnl jnvor jbwv jwnv jvnwrv owc.;mcpo3mm', body: 'webirv j3ekjn jo3en jno mln cko3 ofnle '},
+        {id: '2', title: 'nveonvoevnl jnvor jbwv jwnv jvnwrv owc.;mcpo3mm', body: 'webirv j3ekjn jo3en jno mln cko3 ofnle '},
+        {id: '3', title: 'nveonvoevnl jnvor jbwv jwnv jvnwrv owc.;mcpo3mm', body: 'webirv j3ekjn jo3en jno mln cko3 ofnle '},
+    ]
+}
+
+const rootReducer = (state, action) => {
+    return state
+}
+
+export default rootReducer
